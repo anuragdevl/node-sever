ec3dd60d3d276e5f4c8025284c39abb9224199e8
anuragdevl <anurag.singh0512@gmail.com>
2024-12-17 12:54:22 +0530

    dynamic route

--- src/app.js
+++ src/app.js
@@ -2,14 +2,39 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello from Server")
-})
+app.get("/user/:userId/:name/:password",(req,res)=>{
+    console.log(req.params)
+        res.send({
+            firstName:"ANURAG",
+            lastName:"Singh" 
+        })
+    })
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from Server test")
-})
+    // app.post("/user",(req,res)=>{
+    //     console.log("data post successcefullly ")
+    //     // saving data to db
+    //     res.send( "Data succesfully send to the database")
+    // })
 
+
+    
+    // app.delete("/user",(req,res)=>{
+    //     console.log("data post successcefullly ")
+    //     // saving data to db
+    //     res.send( "Deleted  succesfully")
+    // })
+// app.use("/hello/2",(req,res)=>{
+//     res.send("Hello from Server hello/2 ")
+// })
+
+// app.use("/hello",(req,res)=>{
+//     res.send("Hello from Server hello")
+// })
+
+// app.use("/test",(req,res)=>{
+//     res.send("Hello from Server test")
+// })
+ 
 app.listen(4000,()=>{
     console.log("server Connected Succesfully")
-});
+}); 
